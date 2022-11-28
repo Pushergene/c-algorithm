64d9f7c52fe349245f408c2925be1dcec3849363
Pushergene <pushergene@protonmail.com>
2022-11-28 12:02:48 +0100

    Update main3.cpp

--- main3.cpp
+++ main3.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 void fun (int A[ ]) {
-    cout << sizeof(A) / sizeof(int) << endl;
+    for(int i = 0; i < 5; i++) 
+        cout << A[i] << endl;
 }
 
 int main(){
     int A[ ] = {2,4,6,8,10};
     int n = 5;
     fun(A);
-    cout << sizeof(A) / sizeof(int) << endl;
     for(int x: A)
     cout << x << " ";
 }
